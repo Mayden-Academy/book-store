d817f015893234f40bd09097b7fe07f69bddfe95
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-13 16:21:32 +0000

    added book class

---
+++ app/src/bookClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandrk
+ * Date: 13/11/2017
+ * Time: 16:13
+ */
+
+namespace App\books;
+
+class book
+{
+    public $title;
+    public $price;
+    public $description;
+    public $image;
+}
