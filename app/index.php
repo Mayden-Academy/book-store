63fa40cd2a7c9110416364436e1ef547d4902865
Robbie Wood <githubphagocyte@gmail.com>
2017-11-13 17:13:48 +0000

    Add outline of book list page html.

--- app/index.php
+++ app/index.php
@@ -1,3 +1,47 @@
 <?php
-
 require "vendor/autoload.php";
+?>
+
+<!DOCTYPE html>
+<html lan="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Book List</title>
+</head>
+<body>
+<?php
+include "header.php";
+?>
+<form>
+    <input type="text" placeholder="Type here to search...">
+    <button type="button">Search</button>
+</form>
+
+<div class="filter-column col-xs-3">
+    <h2>Filter by price</h2>
+
+    <!--SAMPLE FILTER BUTTON-->
+    <a href="index.php">£0.00 - £9.99</a>
+
+</div>
+
+<div class="book-list col-xs-9">
+
+    <!--SAMPLE LISTED BOOK-->
+    <div class="listed-book col-xs-4">
+        <img src="">
+        <h3>TITLE</h3>
+        <h4>£5.99</h4>
+        <a href="">More details</a>
+    </div>
+
+
+</div>
+
+<?php
+include "footer.php";
+?>
+</body>
+</html>
