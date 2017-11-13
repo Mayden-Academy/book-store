e245c29673ed3e4a2515bc3e94ab7b6e8d079baf
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-11-13 17:09:09 +0000

    started header

---
+++ app/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/lib/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <title>Booky McBookface</title>
+</head>
+<body>
+<header>
+    <nav class="navbar bg-info">
+        <div class="container-fluid navbar-fixed-top">
+                <div class="navbar-brand">
+                    BMcB
+                </div>
+                <a class="btn navbar-btn btn-primary" href="index.php">
+                    Home
+                </a>
+                <div class="navbar-right">
+                    <div class="navbar-text totalBooks">
+                        <p>11 items</p>
+                    </div>
+                    <div class="navbar-text totalPrice">
+                        <p>£127.60</p>
+                    </div>
+                </div>
+            <a class="navbar-right">
+                <img src="images/trolley.svg" class="trolley"/>
+            </a>
+        </div>
+    </nav>
+</header>
+
+
+
+</body>
+</html>
