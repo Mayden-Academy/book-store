7e99a5b66c10ed7b30e62216adea72030773d811
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-11-14 10:50:42 +0000

    tidied up code

--- app/header.php
+++ app/header.php
@@ -1,34 +1,22 @@
-<!--<!DOCTYPE html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--    <meta charset="UTF-8">-->
-<!--    <link href="css/lib/bootstrap.min.css" type="text/css" rel="stylesheet">-->
-<!--    <link href="css/style.css" type="text/css" rel="stylesheet">-->
-<!--</head>-->
-<!--<body>-->
-<!--<header>/-->
-    <header class="navbar">
-        <div class="container-fluid navbar-fixed-top bg-info">
-                <div class="navbar-brand text-primary">
-                    BMcB
-                </div>
-                <a class="btn navbar-btn btn-primary" href="index.php">
-                    Home
-                </a>
-                <div class="navbar-right">
-                    <div class="navbar-text totalBooks text-primary">
-                        <p>11 items</p>
-                    </div>
-                    <div class="navbar-text totalPrice text-primary">
-                        <p>£127.60</p>
-                    </div>
-                </div>
-<!--            add the link to the cart to this anchor tag-->
-            <a class="navbar-right">
-                <img src="images/trolley.svg" class="trolley"/>
-            </a>
+<header class="navbar">
+    <div class="container-fluid navbar-fixed-top bg-info">
+        <div class="navbar-brand text-primary">
+            BMcB
         </div>
-    </header>
-<!--</header>-->
-<!--</body>-->
-<!--</html>-->
+        <a class="btn navbar-btn btn-primary" href="index.php">
+            Home
+        </a>
+        <div class="navbar-right">
+            <div class="navbar-text totalBooks text-primary">
+                <p>11 items</p>
+            </div>
+            <div class="navbar-text totalPrice text-primary">
+                <p>£127.60</p>
+            </div>
+        </div>
+        <!--            add the link to the cart to this anchor tag-->
+        <a class="navbar-right">
+            <img src="images/trolley.svg" class="trolley"/>
+        </a>
+    </div>
+</header>
