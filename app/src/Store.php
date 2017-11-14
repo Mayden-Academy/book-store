314b2634c776f80e95b6dba2f100dc9621843e88
Ben Long <bennlongg@gmail.com>
2017-11-14 15:11:57 +0000

    show all books function

--- app/src/Store.php
+++ app/src/Store.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App;
+
 Use App\Book as Book;
+
 /**
  * Class Store - prepare and execute query to DB to get all book names, ids, prices and images
  * @package App
@@ -20,11 +22,27 @@ class Store
     /**
      * @param $db - property from DbConnector class
      */
-    public function getAllBooks(){
+    public function getAllBooks()
+    {
         $query = $this->db->prepare("SELECT `id`, `title`, `price`, `image` FROM `books`;");
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, Book::class);
         $books = $query->fetchAll();
         return $books;
+
+    }
+
+    public function showAllBooks($books)
+    {
+        foreach ($books as $book) {
+            echo "<div class='listedBook col-xs-4'>
+                        <a href='../individualBookPage.php?id=$book->id'>
+                            <img class='listedBookImage' src='$book->image'>
+                            <h4 class=''>$book->title</h4>
+                            <h4 class=''>£$book->price</h4>
+                            <p class=''>$book->description</p>
+                        </a>
+                  </div>";
+        }
     }
 }
