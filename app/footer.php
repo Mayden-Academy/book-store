a3cf7b76d2c11946ce489d6acc6c6f3a20e61fc4
Ruth Waterfield <ruth.waterfield@mayden.co.uk>
2017-11-14 09:43:52 +0000

    created footer

---
+++ app/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/lib/bootstrap.min.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<footer>
+    <nav class="bg-info navbar-fixed-bottom">
+        <h1 class="text-primary text-center">Booky McBookface</h1>
+    </nav>
+</footer>
+</body>
+</html>
--- app/header.php
+++ app/header.php
@@ -4,34 +4,31 @@
     <meta charset="UTF-8">
     <link href="css/lib/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="css/style.css" type="text/css" rel="stylesheet">
-    <title>Booky McBookface</title>
 </head>
 <body>
 <header>
-    <nav class="navbar bg-info siteHeader">
-        <div class="container-fluid navbar-fixed-top siteHeaderContent">
-                <div class="navbar-brand">
+    <nav class="navbar">
+        <div class="container-fluid navbar-fixed-top bg-info">
+                <div class="navbar-brand text-primary">
                     BMcB
                 </div>
                 <a class="btn navbar-btn btn-primary" href="index.php">
                     Home
                 </a>
                 <div class="navbar-right">
-                    <div class="navbar-text totalBooks">
+                    <div class="navbar-text totalBooks text-primary">
                         <p>11 items</p>
                     </div>
-                    <div class="navbar-text totalPrice">
+                    <div class="navbar-text totalPrice text-primary">
                         <p>£127.60</p>
                     </div>
                 </div>
+<!--            add the link to the cart to this anchor tag-->
             <a class="navbar-right">
                 <img src="images/trolley.svg" class="trolley"/>
             </a>
         </div>
     </nav>
 </header>
-
-
-
 </body>
 </html>
