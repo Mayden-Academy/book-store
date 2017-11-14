55ea2b773c9605085fd014a6140970383c255502
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-14 11:43:02 +0000

    added docblock

--- app/src/Store.php
+++ app/src/Store.php
@@ -6,10 +6,14 @@ namespace App;
 /**
  * Class Store - prepare and execute query to DB to get all book names, ids, prices and images
  * @package App
- * @return list of all books
+ * @return list(array) of all books
  */
 class Store
 {
+
+    /**
+     * @param $db - property from DbConnector class
+     */
     public function getData($db){
         $query = $db->prepare("SELECT `id`, `title`, `price`, `image` FROM `books`;");
         $query->execute();
