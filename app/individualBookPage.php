c64ca75c73b39e55d6d7dd116ab9389c39eb43e7
Robbie Wood <githubphagocyte@gmail.com>
2017-11-14 11:49:46 +0000

    Update individualBookPage.php to point to renamed css file.

--- app/individualBookPage.php
+++ app/individualBookPage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/lib/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>BOOK PAGE | BOOK STORE</title>
 </head>
 <body>
