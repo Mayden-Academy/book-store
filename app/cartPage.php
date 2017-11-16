21fcf307f035ed5363831387fe9ec006926397c4
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-16 12:27:10 +0000

    add functionality to cart

--- app/cartPage.php
+++ app/cartPage.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 require "../vendor/autoload.php";
 $connection = new \App\DbConnector();
-$db=$connection->getDb();
+$db = $connection->getDb();
 ?>
 
 <!DOCTYPE html>
@@ -15,77 +16,47 @@ $db=$connection->getDb();
 
 <div class="stickyFooterExcluder">
 
-<?php include 'includes/header.php'; ?>
+    <?php include 'includes/header.php'; ?>
 
-<div class="container text-center">
-    <h1>
-        Shopping Cart
-    </h1>
-</div>
-<div class="container">
-    <div class="row">
-        <table class="table col-xs-6">
-            <tr>
-                <th>
-                    Book Title
-                </th>
-                <th>
-                    Price
-                </th>
-                <th>
-                    Remove from cart
-                </th>
-            </tr>
-            <!-- takes the array of book IDs stored in the session and passes them into the Book class to then output title and price into html-->
-            <?php
-                $array = [1,2,3,4,5,2];
+    <div class="container text-center">
+        <h1>Shopping Cart</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <table class="table col-xs-6">
+                <tr>
+                    <th>Book Title</th>
+                    <th>Price</th>
+                    <th>Remove from cart</th>
+                </tr>
+                <!-- takes the array of book IDs stored in the session and passes them into the Book class to then output title and price into html-->
+                <?php
+                $array = $_SESSION["cart"]["bookIds"];
                 sort($array);
-                foreach ($array  as $bookId) {
+                foreach ($array as $bookId) {
                     $book = new \App\Book($db, $bookId);
 
                     echo
-                    " <tr>
-                        <td>
-                            <a href='../individualBookPage.php?id=$book->id'> $book->title</a>
-                        </td>
-                        <td>
-                              £$book->price
-                        </td>
-                        <td>
-                            <button class='btn btn-info glyphicon-minus'>
-                            </button>
-                        </td>
-                       </tr>";
+                    "<tr>
+                        <td><a href='../individualBookPage.php?id=$book->id'> $book->title</a></td>
+                        <td>£$book->price</td>
+                        <td><a class='btn btn-info glyphicon-minus' href='#'></a></td>
+                     </tr>";
                 }
-            ?>
-        </table>
+                ?>
+            </table>
+        </div>
     </div>
-</div>
-<div class="container">
-    <!--    container-->
-    <div>
-        <p><b>
-        <span class="col-xs-2">
-            Books in cart
-        </span>
-                <span class="col-xs-1">
-            1
-        </span>
+    <div class="container">
+        <h5>
+            <span class="col-xs-2">Books in cart:</span>
+            <span class="col-xs-1"><?php echo (empty($_SESSION)) ? "0" : $_SESSION["cart"]["totalBooks"]; ?></span>
+        </h5>
+        <h5>
+            <span class="col-xs-2">Total price:</span>
+            <span class="col-xs-1">£<?php echo (empty($_SESSION)) ? "0.00" : $_SESSION["cart"]["totalPrice"]; ?></span>
+        </h5>
     </div>
-    <div>
-        <p>
-        <span class="col-xs-2">
-            Total price
-        </span>
-            <span class="col-xs-1">
-            £375.00
-        </span>
-            </b>
-        </p>
-    </div>
-
-</div>
-
 </div>
 
 <?php include 'includes/footer.php'; ?>
