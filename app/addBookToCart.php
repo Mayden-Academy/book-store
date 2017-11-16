8645195aae00dde605059458fe3170d6c09c446b
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-16 18:04:49 +0000

    remove redundunt variables

--- app/addBookToCart.php
+++ app/addBookToCart.php
@@ -3,8 +3,7 @@ require "../vendor/autoload.php";
 session_start();
 if (isset($_GET['id']) && isset($_GET['price'])) {
     $id = $_GET['id'];
-    $price = $_GET['price'];
-    $_SESSION = App\Cart::addBookToCart($_SESSION, $id, $price);
+    $_SESSION = App\Cart::addBookToCart($_SESSION, $id, $_GET['price']);
     header('Location: individualBookPage.php?id=' . $id);
     die();
 }
