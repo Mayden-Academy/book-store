327422be185789ee1c028834696913fd50673b9d
Ismael Velasco <mayel.vel@gmail.com>
2017-11-16 16:37:01 +0000

    refactored cart Page to incorporate Cart changes

--- app/cartPage.php
+++ app/cartPage.php
@@ -2,7 +2,8 @@
 session_start();
 require "../vendor/autoload.php";
 $connection = new \App\DbConnector();
-$db=$connection->getDb();
+$db = $connection->getDb();
+$bookIds = $_SESSION['cart']['bookIds'];
 session_start();
 ?>
 
@@ -17,76 +18,71 @@ session_start();
 
 <div class="stickyFooterExcluder">
 
-<?php include 'includes/header.php'; ?>
+    <?php include 'includes/header.php'; ?>
 
-<div class="container text-center">
-    <h1>
-        Shopping Cart
-    </h1>
-</div>
-<div class="container">
-    <div class="row">
-        <table class="table col-xs-6">
-            <tr>
-                <th>
-                    Book Title
-                </th>
-                <th>
-                    Price
-                </th>
-                <th>
-                    Remove from cart
-                </th>
-            </tr>
-            <!-- takes the array of book IDs stored in the session and passes them into the Book class to then output title and price into html-->
-            <?php
-                $array = [1,2,3,4,5,2];
-                sort($array);
-                foreach ($array  as $bookId) {
-                    $book = new \App\Book($db, $bookId);
-
-                    echo
-                    " <tr>
-                        <td>
-                            <a href='../individualBookPage.php?id=$book->id'> $book->title</a>
-                        </td>
-                        <td>
-                              £$book->price
-                        </td>
-                        <td>
-                            <button class='btn btn-info glyphicon-minus'>
-                            </button>
-                        </td>
-                       </tr>";
-                }
-            ?>
-        </table>
+    <div class="container text-center">
+        <h1>
+            Shopping Cart
+        </h1>
     </div>
-</div>
-<div class="container">
-    <!--    container-->
-    <div>
-        <p><b>
+    <div class="container">
+        <div class="row">
+            <table class="table col-xs-6">
+                <tr>
+                    <th>
+                        Book Title
+                    </th>
+                    <th>
+                        Price
+                    </th>
+                    <th>
+                        Remove from cart
+                    </th>
+                </tr>
+                <!-- takes the array of book IDs stored in the session and passes them into the Book class to then output title and price into html-->
+                <?php
+                sort($bookIds);
+                foreach ($bookIds as $bookId) {
+                    $book = new \App\Book($db, $bookId); ?>
+                    <tr>
+                        <td>
+                            <a href='individualBookPage.php?id=<?php echo $book->id; ?>'> <?php echo $book->title; ?></a>
+                        </td>
+                        <td>
+                            <?php echo $book->displayPrice(); ?>
+                        </td>
+                        <td>
+                            <button class='btn btn-info glyphicon-minus'></button>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+    <div class="container">
+        <!--    container-->
+        <div>
+            <p><b>
         <span class="col-xs-2">
             Books in cart
         </span>
-                <span class="col-xs-1">
+                    <span class="col-xs-1">
             1
         </span>
-    </div>
-    <div>
-        <p>
+        </div>
+        <div>
+            <p>
         <span class="col-xs-2">
             Total price
         </span>
-            <span class="col-xs-1">
+                <span class="col-xs-1">
             £375.00
         </span>
-            </b>
-        </p>
-    </div>
+                </b>
+            </p>
+        </div>
 
-</div>
+    </div>
 
 </div>
 
