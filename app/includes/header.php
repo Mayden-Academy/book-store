2c8fc9a1e65d5b42140b2b87344a7b285b86875a
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-17 11:18:03 +0000

    changed isset to empty for session check

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,10 +8,10 @@
         </a>
         <div class="navbar-right">
             <div class="navbar-text totalBooks text-primary">
-                <p>Books: <?php echo (isset($_SESSION)) ? "0": $_SESSION["cart"]["totalBooks"];?></p>
+                <p>Books: <?php echo (empty($_SESSION)) ? "0": $_SESSION["cart"]["totalBooks"];?></p>
             </div>
             <div class="navbar-text totalPrice text-primary">
-                <p>£<?php echo (isset($_SESSION)) ? "0.00": $_SESSION["cart"]["totalPrice"];?></p>
+                <p>£<?php echo (empty($_SESSION)) ? "0.00": $_SESSION["cart"]["totalPrice"];?></p>
             </div>
         </div>
         <a class="navbar-right" href="cartPage.php">
